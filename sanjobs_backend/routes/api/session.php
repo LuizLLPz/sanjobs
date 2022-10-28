28e31be014464f0bcdfcbf76e1da377279bdef3a
LuizLLPz <luizllp321@gmail.com>
2022-10-28 10:23:28 -0300

    Adicionando arquivo único para tratar endpoint de sessão

--- sanjobs_backend/routes/api/session.php
+++ sanjobs_backend/routes/api/session.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -17,17 +18,19 @@ use Illuminate\Support\Facades\Redis;
 |
 */
 
-class APIResponse {
-    public String $message;
+class APIResponse
+{
+    public string $message;
     public mixed $data;
 }
+
 
 Route::get('/session', function (Request $request) {
     $resp = new APIResponse();
     $sid = $request->cookie('sid');
     if (!$sid) {
         $resp->message = 'Bad Request';
-        return response (
+        return response(
             json_encode($resp),
             400);
     }
@@ -35,7 +38,7 @@ Route::get('/session', function (Request $request) {
     $uid = Redis::get($sid);
     if (!$uid) {
         $resp->message = 'Session Expired';
-        return response (
+        return response(
             json_encode($resp),
             401);
     }
@@ -43,14 +46,14 @@ Route::get('/session', function (Request $request) {
     $user = DB::table('User')->where('id', $uid)->limit(1)->first();
     if (!$user) {
         $resp->message = 'User not found!';
-        return response (
+        return response(
             json_encode($resp),
             404
         );
     }
     $resp->message = 'OK';
     $resp->data = $user;
-    return response (
+    return response(
         json_encode($resp)
     );
 
@@ -63,7 +66,7 @@ Route::post('/session', function (Request $request) {
     $password = $request->input('password');
     if (!$email || !$password) {
         $resp->message = 'Bad Request';
-        return response (
+        return response(
             json_encode($resp),
             400);
     }
@@ -71,7 +74,7 @@ Route::post('/session', function (Request $request) {
     $user = DB::table('User')->where('email', $email)->limit(1)->first();
     if (!$user) {
         $resp->message = 'User not found!';
-        return response (
+        return response(
             json_encode($resp),
             404
         );
@@ -87,11 +90,11 @@ Route::post('/session', function (Request $request) {
 
     $resp->message = 'Logged in Successfully!';
     $resp->data = $user;
-    $sid = uniqid().$user->name;
+    $sid = uniqid() . $user->name;
     Redis::set($sid, $user->id);
     return response(
         json_encode($resp)
-    )->cookie('sid', $sid, 2*24*60);
+    )->cookie('sid', $sid, 2 * 24 * 60);
 });
 
 
@@ -100,7 +103,7 @@ Route::delete('/session', function (Request $request) {
     $sid = $request->cookie('sid');
     if (!$sid) {
         $resp->message = 'Bad Request';
-        return response (
+        return response(
             json_encode($resp),
             400);
     }
@@ -108,7 +111,7 @@ Route::delete('/session', function (Request $request) {
     $uid = Redis::get($sid);
     if (!$uid) {
         $resp->message = 'Session Already Expired';
-        return response (
+        return response(
             json_encode($resp),
             401);
     }

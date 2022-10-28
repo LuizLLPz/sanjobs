2923679d0120563029a8f8f738811bba5757b65c
LuizLLPz <luizllp321@gmail.com>
2022-10-28 12:56:16 -0300

    Removing incoherent comment

--- sanjobs_backend/routes/api/session.php
+++ sanjobs_backend/routes/api/session.php
@@ -6,17 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redis;
 use App\Helpers\APIResponse;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 
 
 Route::get('/session', function (Request $request) {

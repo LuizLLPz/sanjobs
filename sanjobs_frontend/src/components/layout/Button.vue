429005b4dc280d3a6fcc6bf03e8984252174377a
LuizLLPz <luizllp321@gmail.com>
2022-11-05 10:16:55 -0300

    Component simples para botão

---
+++ sanjobs_frontend/src/components/layout/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonMain",
+  props: {
+    label: String
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: v-bind('h ? h : "30px"');
+    width: v-bind('w ? w : "125px"');
+    color: var(--color-secondary);
+    background-color: var(--color-primary);
+    border: none;
+    padding: 10px;
+    border-radius: v-bind('br ? br : "14px"');
+    margin: 5px;
+  }
+</style>

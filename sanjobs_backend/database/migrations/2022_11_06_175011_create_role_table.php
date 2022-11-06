59357085158792f2a1ad0679a7efc4f70a8d9915
LuizLLPz <luizllp321@gmail.com>
2022-11-06 15:19:05 -0300

    Adding role table for company creation and adding job table with skills and related tables

---
+++ sanjobs_backend/database/migrations/2022_11_06_175011_create_role_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Role', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+        });
+
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('User');
+            $table->foreignId('role_id')->constrained('Role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Role');
+        Schema::dropIfExists('user_role');
+    }
+};
---
+++ sanjobs_backend/database/migrations/2022_11_06_175636_create_job_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Job', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 75);
+            $table->text('description');
+            $table->dateTime('post_date');
+            $table->foreignId('company_id');
+            $table->foreign('company_id')->references('id')->on('User');
+            $table->foreignId('city_id');
+            $table->foreign('city_id')->references('id')->on('City');
+        });
+
+        Schema::create('Area', function(Blueprint $table) {
+            $table->id();
+            $table->string('name', 75);
+        });
+
+        Schema::create('Skill', function(Blueprint $table) {
+            $table->id();
+            $table->string('name', 75);
+            $table->tinyInteger('level');
+
+        });
+
+        Schema::create('job_tag', function(Blueprint $table) {
+            $table->foreignId('job_id');
+            $table->foreign('job_id')->references('id')->on('Job');
+            $table->foreignId('tag_id');
+            // use tag id for both area and skill without diuplicate error
+            $table->foreign('tag_id', 'job_tag_area_id_foreign')->references('id')->on('Area');
+            $table->foreign('tag_id', 'job_tag_skill_id_foreign')->references('id')->on('Skill');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Job');
+        Schema::dropIfExists('Area');
+        Schema::dropIfExists('Skill');
+        Schema::dropIfExists('job_tag');
+    }
+};

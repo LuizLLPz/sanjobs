c5667359ba45ca36b140b79b5cfcf00cb909d93b
LuizLLPz <luizllp321@gmail.com>
2022-11-12 15:21:25 -0300

    Add job api consumption

--- sanjobs_frontend/src/views/JobsPage.vue
+++ sanjobs_frontend/src/views/JobsPage.vue
@@ -2,7 +2,8 @@
   <div class="center">
     <MainHeading text="Vagas disponíveis"/>
     <div class="job_container">
-      <JobCard v-for="job of jobs" v-bind:job="job" v-bind:key="job"/>
+      <JobCard v-for="job of jobs"
+               v-bind:job="job" v-bind:key="job"/>
     </div>
   </div>
 
@@ -10,15 +11,40 @@
 
 <script>
 import MainHeading from "@/components/typography/MainHeading";
+import JobCard from "@/components/jobs/JobCard";
+
 export default {
   name: "JobsPage",
   components: {
-    MainHeading
+    MainHeading,
+    JobCard
+  },
+  data: function () {
+    return {
+      jobs: []
+    }
+  },
+  methods: {
+    fetchJobs: async function () {
+      const response = await fetch("http://localhost:8000/api/jobs");
+      this.jobs = await response.json();
+      this.jobs = this.jobs.data;
+    }
+  },
+  mounted: function () {
+    this.fetchJobs();
   }
 }
 
 </script>
 
 <style scoped>
-
+  .job_container {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+  }
 </style>

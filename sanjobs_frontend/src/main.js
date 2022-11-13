aaca05980a51a336e413259a6303c568b5d08834
LuizLLPz <luizllp321@gmail.com>
2022-11-13 15:19:12 -0300

    User page view and added to router

--- sanjobs_frontend/src/main.js
+++ sanjobs_frontend/src/main.js
@@ -4,11 +4,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import IndexPage from "@/views/IndexPage";
 import RegisterPage from "@/views/RegisterPage";
 import JobsPage from "@/views/JobsPage";
+import UserPage from "@/views/UserPage";
 
 const routes = [
     { path: '/', component: IndexPage },
     { path: '/register', component: RegisterPage },
     { path: '/jobs', component: JobsPage },
+    { path: '/user', component: UserPage },
 ]
 
 const router = createRouter({

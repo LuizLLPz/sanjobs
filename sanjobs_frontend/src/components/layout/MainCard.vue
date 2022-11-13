09c6f4c954d7fc0c1842a135b6d1ab78c73076b4
LuizLLPz <luizllp321@gmail.com>
2022-11-13 15:25:19 -0300

    Main card script setup

--- sanjobs_frontend/src/components/layout/MainCard.vue
+++ sanjobs_frontend/src/components/layout/MainCard.vue
@@ -1,10 +1,22 @@
 <template>
-
+  <div class="main_card">
+    <MainHeading v-bind:title="title" />
+  </div>
 </template>
 
 <script>
+import MainHeading from "@/components/typography/MainHeading";
+
 export default {
-  name: "MainCard"
+  name: "MainCard",
+  components: {
+    MainHeading
+  },
+  props: {
+    title: String,
+    scheme: String
+  }
+
 }
 </script>
 
